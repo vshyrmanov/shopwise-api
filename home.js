965be9a0902aa03a831379df990edef605ceac66
vshyrmanov <shyrmanovvm@gmail.com>
2023-03-07 23:34:50 +0200

    test

--- home.js
+++ home.js
@@ -1,10 +1,14 @@
 const express = require("express");
 const router = express.Router();
+require('dotenv').config()
+
 
 router.get("/", async (req, res, next) => {
 	return res.status(200).json({
 		title: "Express Testing",
 		message: "The app is working properly! ))",
+		test: process.env.METRO,
+		test2: process.env.FOZZY,
 	});
 });
 
