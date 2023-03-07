7e6eca6e1d239619b558ccec1bf0dcabff0894c7
vshyrmanov <shyrmanovvm@gmail.com>
2023-03-07 23:46:21 +0200

    test

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 require('dotenv').config()
 const express = require('express');
 const app = express()
+const cors = require('cors');
 const Products = require('./Routes/Product.routes');
 const home = require('./home');
 
 const PORT = process.env.PORT || 80;
 app.use(express.json());
+app.use(cors());
 
 app.use('/product', Products);
 app.use('/home', home);
