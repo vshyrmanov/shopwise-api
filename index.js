6413e8bfaa131b73d32c9e94498a909a16674422
vshyrmanov <shyrmanovvm@gmail.com>
2023-03-07 23:19:20 +0200

    add home test page

--- index.js
+++ index.js
@@ -2,12 +2,13 @@
 const express = require('express');
 const app = express()
 const Products = require('./Routes/Product.routes');
+const home = require('./home');
+
 const PORT = 5000;
-app.use(express.json())
+app.use(express.json());
 
-
-
-app.use('/product', Products)
+app.use('/product', Products);
+app.use('/home', home);
 
 // start()
 
