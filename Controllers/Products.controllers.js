453f49c51a8ce7e00e762c59484b707f81e4fef9
vshyrmanov <shyrmanovvm@gmail.com>
2023-03-08 12:47:45 +0200

    test

--- Controllers/Products.controllers.js
+++ Controllers/Products.controllers.js
@@ -84,6 +84,7 @@ if (process.env.AWS_LAMBDA_FUNCTION_VERSION) {
 const getMetro = async function (search) {
 	let options = {}
 	if (process.env.AWS_LAMBDA_FUNCTION_VERSION) {
+		console.log("IN LAMBDA")
 		options = {
 			args: [...chrome.args, "--hide-scrollbars", "--disable-web-security"],
 			defaultViewport: chrome.defaultViewport,
@@ -103,64 +104,68 @@ const getMetro = async function (search) {
 	// })
 	const browser = await puppeteer.launch(options);
 	// const browser = await puppeteer.launch( {headless: true} );
-	const page = await browser.newPage();
-	await page.goto(`https://metro.zakaz.ua/uk/search/?q=${search}`)
-	const data = await page.evaluate(function () {
-		const events = document.querySelectorAll(".ProductTile__details")
-		const array = [];
-		for (let x = 0; x < events.length; x++) {
-			const test = events[x].innerText.split(/\r?\n/)
-			if (test.includes("Немає в наявності")) {
-				array.push({
-					price: test[1],
-					available: false,
-					title: test[1],
-					owner: "Metro"
-				})
-				break;
-			}
-			if (test.length === 3) {
-				array.push({
-						price: test[0],
-						title: test[1],
-						owner: "Metro",
-						available: true,
-				})
-			}
-			if (test.length === 4) {
-				array.push({
-						price: test[0],
-						discount: test[1],
-						title: test[2],
-						owner: "Metro",
-						available: true,
-				})
-			}
-		}
-
-		return array;
-	})
-	return data
+	let page = await browser.newPage();
+	await page.goto(`https://www.google.com/`)
+	return await page.title();
+	// const page = await browser.newPage();
+	// await page.goto(`https://metro.zakaz.ua/uk/search/?q=${search}`)
+	// const data = await page.evaluate(function () {
+	// 	const events = document.querySelectorAll(".ProductTile__details")
+	// 	const array = [];
+	// 	for (let x = 0; x < events.length; x++) {
+	// 		const test = events[x].innerText.split(/\r?\n/)
+	// 		if (test.includes("Немає в наявності")) {
+	// 			array.push({
+	// 				price: test[1],
+	// 				available: false,
+	// 				title: test[1],
+	// 				owner: "Metro"
+	// 			})
+	// 			break;
+	// 		}
+	// 		if (test.length === 3) {
+	// 			array.push({
+	// 					price: test[0],
+	// 					title: test[1],
+	// 					owner: "Metro",
+	// 					available: true,
+	// 			})
+	// 		}
+	// 		if (test.length === 4) {
+	// 			array.push({
+	// 					price: test[0],
+	// 					discount: test[1],
+	// 					title: test[2],
+	// 					owner: "Metro",
+	// 					available: true,
+	// 			})
+	// 		}
+	// 	}
+	//
+	// 	return array;
+	// })
+	// return data
 };
 
-const generateList = async (search) => {
-	const dataFozzy = await getFozzy(search);
-	const dataMetro = await getMetro(search);
-	const dataNovus = await getNovus(search);
-	const unionArray = {
-		result: [
-			...dataFozzy,
-			...dataMetro,
-			...dataNovus]
-	};
-	return unionArray;
-}
+// const generateList = async (search) => {
+// 	// const dataFozzy = await getFozzy(search);
+// 	const dataMetro = await getMetro(search);
+// 	// const dataNovus = await getNovus(search);
+// 	const unionArray = {
+// 		result: [
+// 			// ...dataFozzy,
+// 			...dataMetro,
+// 			// ...dataNovus
+// 		]
+// 	};
+// 	return unionArray;
+// }
 
 const controller = {
 	search: async (req, res) => {
 		try {
 			const { search } = req.body;
-			const dataMetro = await getMetro(search);
+			const dataMetro = await getMetro();
 			// const result = await generateList(search)
 			res.json(dataMetro)
 		} catch (e) {
