118d7618fdc219e28e7328615dd462ad3a69d300
vshyrmanov <shyrmanovvm@gmail.com>
2023-03-08 13:54:38 +0200

    test

--- index.js
+++ index.js
@@ -25,15 +25,15 @@ if (process.env.AWS_LAMBDA_FUNCTION_VERSION) {
 app.get("/api", async (req, res) => {
 	let options = {};
 
-	if (process.env.AWS_LAMBDA_FUNCTION_VERSION) {
+	// if (process.env.AWS_LAMBDA_FUNCTION_VERSION) {
 		options = {
-			args: [...chrome.args, "--hide-scrollbars", "--disable-web-security", '--no-sandbox', '--disable-setuid-sandbox'],
+			args: [ "--hide-scrollbars", "--disable-web-security", '--no-sandbox', '--disable-setuid-sandbox'],
 			defaultViewport: chrome.defaultViewport,
 			executablePath: await chrome.executablePath,
 			headless: true,
 			ignoreHTTPSErrors: true,
 		};
-	}
+	// }
 
 	try {
 		let browser = await puppeteer.launch(options);
